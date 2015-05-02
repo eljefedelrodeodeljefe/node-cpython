dbfd9267b330145b7b15090fa26a72ed16039507
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-05-02 14:23:57 +0200

    FIX build failed due to wrong binding.gyp and missing src files

---
+++ src/hello.cc
@@ -0,0 +1,15 @@
+#include <nan.h>
+#include <Python.h>
+
+using namespace v8;
+
+NAN_METHOD(Method) {
+  NanScope();
+  NanReturnValue(NanNew("world"));
+}
+
+void Init(Handle<Object> exports) {
+  exports->Set(NanNew("hello"), NanNew<FunctionTemplate>(Method)->GetFunction());
+}
+
+NODE_MODULE(hello, Init)
