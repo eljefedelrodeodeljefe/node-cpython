adcd0581e8059bcf82095718ff77090be9d4df54
Robert Jefe Lindstädt <robert.lindstaedt@gmail.com>
2015-05-23 13:56:39 +0200

    FIX: install script now working from project root.
    On `npm pre-install` this script will run and isntall python dependencies into the right folder structure. This is necessary for python scripts to find compiled pylibs.
    
    The user can spot install errors and other foo through stream output into stdout and stderr. For your reference you'll also see pretty prints of running times of the total cript and parts of it.
    
    No magic here

--- install.js
+++ install.js
@@ -7,79 +7,70 @@ var ee = new EventEmitter();
 var colors = require('./bin/colors.js');
 
 
-// var configureOpts = ["--prefix=/Users/robert.lindstaedt/Documents/repos/node-cpython/deps/2.7/build",
-//                      "--exec-prefix=/Users/robert.lindstaedt/Documents/repos/node-cpython/deps/2.7/build",
-//                      "--without-PACKAGE"];
-//
-// // console.log('Starting directory: ' + process.cwd());
-// // try {
-// //   process.chdir('deps/2.7/cpython');
-// //   console.log('New directory: ' + process.cwd());
-// // }
-// // catch (err) {
-// //   console.log('chdir: ' + err);
-// // }
-// console.log('Spawn: started');
-// var configure = spawn('./configure', configureOpts, {cwd: "./deps/2.7/cpython"})
-//
-// console.log('Configure: started');
-// configure.stdout.on('data', function (data) {
-//   console.log('stdout: ' + data);
-// });
-//
-// configure.stderr.on('data', function (data) {
-//   console.log('stderr: ' + data);
-// });
-//
-// configure.on('close', function (code) {
-//   console.log('Configure: child process exited with code ' + code);
-//   ee.emit('done:configure')
-// });
-//
-// ee.on('done:configure', function() {
-//   console.log('Make: started');
-//   var make = spawn('make', {cwd: "deps/2.7/cpython"})
-//
-//   make.stdout.on('data', function (data) {
-//     console.log('stdout: ' + data);
-//   });
-//
-//   make.stderr.on('data', function (data) {
-//     console.log('stderr: ' + data);
-//   });
-//
-//   make.on('close', function (code) {
-//     console.log('Make: child process exited with code ' + code);
-//     ee.emit('done:make')
-//   });
-// })
-//
-//
-// ee.on('done:make', function() {
-//   console.log('Make install: started');
-//   var makeInstall = spawn('make install', {cwd: "deps/2.7/cpython"})
-//   makeInstall.stdout.on('data', function (data) {
-//     console.log('stdout: ' + data);
-//   });
-//
-//   makeInstall.stderr.on('data', function (data) {
-//     console.log('stderr: ' + data);
-//   });
-//
-//   makeInstall.on('close', function (code) {
-//     console.log('Make install: child process exited with code ' + code);
-//     ee.emit('done:makeInstall')
-//   });
-// })
-// ee.emit('done:make');
-//
-// // ee.on('done:makeInstall', function () {
-// //   console.log('Changing workdir back from: ' + process.cwd());
-// //   try {
-// //     process.chdir('../../..');
-// //     console.log('Workdir now: ' + process.cwd());
-// //   }
-// //   catch (err) {
-// //     console.log('chdir: ' + err);
-// //   }
-// // })
+
+
+/*
+* This is the install script which runs on 'npm pre-install'.
+* Not much magic. It just `./configure`s, `make && make install`s inside cpython directory.
+* Most of the code here presents the users output and errors into the console and pretty-prints stuff.
+*
+*/
+
+var configureOpts = ["--prefix=" + process.cwd() + "/deps/2.7/build",
+                     "--exec-prefix=" + process.cwd() + "/deps/2.7/build",
+                     "--with-PACKAGE=no"];
+
+var installTime = process.hrtime();
+var time = process.hrtime();
+var configure = spawn('./configure',configureOpts, {cwd: "deps/2.7/cpython"})
+
+configure.stdout.on('data', function (data) { console.log(colors + data); });
+configure.stderr.on('data', function (data) { console.log(colors+ 'ERR: ' + data); });
+
+configure.on('close', function (code) {
+  var diff = process.hrtime(time);
+  var prettyTime = '[' + ('0' + ~~(diff[0] / 60)).slice(-2) + ':' + ( '0'+ diff[0] % 60).slice(-2) + '] mm:ss\n'
+  console.log('\n\n\n' + colors + 'Configure: child process exited with code after' + code + '\n' + colors + 'after ' + prettyTime + '\n\n');
+  ee.emit('done:configure')
+});
+
+
+
+ee.on('done:configure', function() {
+  var time = process.hrtime();
+  var make = spawn('make', {cwd: "deps/2.7/cpython"})
+
+  make.stdout.on('data', function (data) { console.log(colors + data); });
+  make.stderr.on('data', function (data) { console.log(colors+ 'ERR: ' + data); });
+
+  make.on('close', function (code) {
+    var diff = process.hrtime(time);
+    var prettyTime = '[' + ('0' + ~~(diff[0] / 60)).slice(-2) + ':' + ( '0'+ diff[0] % 60).slice(-2) + '] mm:ss\n'
+    console.log('\n\n\n' + colors + 'Make: child process exited with code ' + code + '\n'+ colors + 'after ' + diff + '\n\n');
+    ee.emit('done:make')
+  });
+})
+
+
+ee.on('done:make', function() {
+  var time = process.hrtime();
+  var makeInstall = spawn('make', ['install'], {cwd: "deps/2.7/cpython"})
+
+  makeInstall.stdout.on('data', function (data) { console.log(colors + data); });
+  makeInstall.stderr.on('data', function (data) { console.log(colors+ 'ERR: ' + data); });
+
+  makeInstall.on('close', function (code) {
+    var diff = process.hrtime(time);
+    var prettyTime = '[' + ('0' + ~~(diff[0] / 60)).slice(-2) + ':' + ( '0'+ diff[0] % 60).slice(-2) + '] mm:ss\n'
+    console.log('\n\n\n' + colors + 'Make install: child process exited with code ' + code + '\n'+ colors + 'after ' + prettyTime + '\n\n');
+    ee.emit('done:makeInstall')
+  });
+})
+
+
+ee.on('done:makeInstall', function() {
+  var installDiff = process.hrtime(installTime);
+  // pretty hrtime -> [$mm:$ss] mm:ss
+  var prettyTime = '[' + ('0' + ~~(installDiff[0] / 60)).slice(-2) + ':' + ( '0'+ installDiff[0] % 60).slice(-2) + '] mm:ss\n'
+  console.log('\n\n\n' + colors + 'Install took ' + prettyTime + '\n\n');
+})
