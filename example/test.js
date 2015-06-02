a4930c1a0acf0409047f84eebae2fb7c3d84c51f
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-06-03 00:49:27 +0200

    intermediate commit: doing really fine
    Finished setting argv

--- example/test.js
+++ example/test.js
@@ -8,4 +8,4 @@ module.on('error', function(err) {console.log(err);})
 module.simpleString("from time import time,ctime\nprint('Today is',ctime(time()))\n")
 
 module.simpleFile("example/multiply_2.py", "multiply_2")
-//module.run()
+module.run()
