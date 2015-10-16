fb96090824ade614e2d62eb0a4c8a83e25e6a527
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-10-16 20:35:19 +0200

    bootstrapped es5 class

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 'use strict'
-/**
-* Entry point into the module. TODO: as soon as es6 modules are supported manually
-* detect its support and require conditionally
-*/
-module.exports = require('./src/es5/cpython')
+module.exports = require('./src/node-cpython')
--- lib/includes/simple_String.h
+++
@@ -1,6 +0,0 @@
-#ifndef SIMPLE_STRING_H
-#define SIMPLE_STRING_H
-
-void simple_String(void);
-
-#endif
