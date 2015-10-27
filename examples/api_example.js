03431738547f03b76d3da05c4a15dc5537acadb5
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-10-28 00:47:09 +0100

    revised docs in example; not passing through fn name and file name

--- examples/api_example.js
+++ examples/api_example.js
@@ -17,20 +17,20 @@ var module = require('../index');
 var Readable = require('stream').Readable;
 
 var rs = new Readable({ "objectMode": true });
-rs.push([3,2]);
-rs.push([7,8]);
-rs.push([7,8]);
-rs.push([7,8]);
-rs.push([7,8]);
+rs.push([1,2]);
+rs.push([2,3]);
+rs.push([3,4]);
+rs.push([4,5]);
+rs.push([5,6]);
 rs.push(null);
 
 
 
 
 module.ffi
-  .require('multiply.py', { path: '/Users/Jefe/Documents/repos/node-cpython/examples' }) // intitializes python
-  .init(rs /*, options {objectMode: true}*/)
-  .run() // expects events from above
+  .require('multiply.py', { path: '/Users/Jefe/Documents/repos/node-cpython/examples' }) // load the python script and intitialize the python interpreter
+  .init(rs) // this epects the stream to be in { objectMode: true }
+  .run('multiply') // Tell node what function to excute.
   .pipe(/*some transform*/) // add your own transform or any other stream here
   .on('end', function() {
     console.log('Ended python context here.');
