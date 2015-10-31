a9e989ca0f13ed4c89cd52c65eb7206da373a2c8
eljefedelrodeodeljefe <robert.lindstaedt@gmail.com>
2015-10-31 15:48:43 +0100

    added gif tutorial; removed logo

--- examples/api_example.js
+++ examples/api_example.js
@@ -15,7 +15,6 @@ var ncpy = require('../index');
 // module.repl()
 
 var Readable = require('stream').Readable;
-
 var SomeStream = new Readable({ "objectMode": true })
 
 SomeStream.push([1,2])
@@ -25,19 +24,21 @@ SomeStream.push([4,50])
 SomeStream.push([55,66])
 SomeStream.push(null)
 
-
-
-
 ncpy.ffi
-  .require('multiplication.py', { path: './examples' }) // load the python script and intitialize the python interpreter
-  .init(SomeStream) // this epects the stream to be in { objectMode: true }
-  .run('multiply') // Tell `ncpy` what function to excute.
-  .pipe(/*some transform*/) // add your own transform or any other stream here
+  // load the python script and intitialize the python interpreter
+  .require('multiplication.py', { path: './examples' })
+  // this expects a stream (in { objectMode: true })
+  .init(SomeStream)
+  // Tell `ncpy` what function to excute.
+  .run('multiply')
+  // add your own transform or any other stream here
+  .pipe(/*some transform*/)
   .on('end', function() {
     console.log('ncpy -> Ending python context here.');
   })
 
-ncpy.ffi('multiplication.py', 'multiply', [], function (err, res) {
+
+ncpy.ffi('multiplication.py', 'multiply', [ 20, 5], function (err, res) {
   console.log('ncpy -> easy call to multiply, here');
   console.log('ncpy -> ' + res + '\n');
 })
